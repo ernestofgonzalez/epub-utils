651894930e063b338f3e8cd5f31a89a8224bdc84
Ernesto F. González <ernestogonzalezz@yahoo.com>
2025-04-29 16:29:48 +0200

    Implement `-v, --version` option

--- epub_utils/cli.py
+++ epub_utils/cli.py
@@ -1,9 +1,37 @@
 import click
-import json
+
+import epub_utils
+
+
+VERSION = "0.0.0a1"
+
+
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(VERSION)
+    ctx.exit()
+
 
 @click.group(
     context_settings=dict(help_option_names=["-h", "--help"]),
 )
+@click.option(
+    '-v', '--version', 
+    is_flag=True, 
+    callback=print_version,
+    expose_value=False, 
+    is_eager=True,
+)
 def main():
     pass
 
+@main.command()
+@click.argument(
+    'path', 
+    type=click.Path(exists=True, file_okay=True),
+    required=True,
+)
+def toc(path):
+    """Outputs the Table of Contents (TOC) of the EPUB file."""
+    doc = epub_utils.Document(path)
