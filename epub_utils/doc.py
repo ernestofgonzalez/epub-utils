68b5c4bedff5235c08d79ae22c943e6788be902d
Ernesto F. González <ernestogonzalezz@yahoo.com>
2025-05-04 16:55:53 +0200

    DRY unzip and read

--- epub_utils/doc.py
+++ epub_utils/doc.py
@@ -4,6 +4,7 @@ from typing import Union
 
 from epub_utils.container import Container
 from epub_utils.package import Package
+from epub_utils.toc import TableOfContents
 
 
 class Document:
@@ -14,6 +15,7 @@ class Document:
         path (Path): The path to the EPUB file.
         _container (Container): The parsed container document.
         _package (Package): The parsed package document.
+        _toc (TableOfContents): The parsed table of contents document.
     """
 
     CONTAINER_FILE_PATH = "META-INF/container.xml"
@@ -30,38 +32,51 @@ class Document:
             raise ValueError(f"Invalid EPUB file: {self.path}")
         self._container: Container = None
         self._package: Package = None
-        self._unzip_and_parse_container()
+        self._toc = TableOfContents = None
 
-    def _unzip_and_parse_container(self) -> None:
+    def _read_file_from_epub(self, file_path: str) -> str:
         """
-        Unzips the EPUB file and parses the container document.
+        Read and decode a file from the EPUB archive.
+
+        Args:
+            file_path (str): Path to the file within the EPUB archive.
+
+        Returns:
+            str: Decoded contents of the file.
+
+        Raises:
+            ValueError: If the file is missing from the EPUB archive.
         """
         with zipfile.ZipFile(self.path, 'r') as epub_zip:
-            if self.CONTAINER_FILE_PATH not in epub_zip.namelist():
-                raise ValueError("Missing container.xml in EPUB file.")
-            container_xml_content = epub_zip.read(self.CONTAINER_FILE_PATH).decode("utf-8")
-            self._container = Container(container_xml_content)
+            if file_path not in epub_zip.namelist():
+                raise ValueError(f"Missing {file_path} in EPUB file.")
+            return epub_zip.read(file_path).decode("utf-8")
 
     @property
     def container(self) -> Container:
         if self._container is None:
-            self._unzip_and_parse_container()
+            container_xml_content = self._read_file_from_epub(self.CONTAINER_FILE_PATH)
+            self._container = Container(container_xml_content)
         return self._container
     
-    def _unzip_and_parse_package(self, package_file_path) -> None:
-        with zipfile.ZipFile(self.path, 'r') as epub_zip:
-            if package_file_path not in epub_zip.namelist():
-                raise ValueError(f"Missing {package_file_path} in EPUB file.")
-            package_xml_content = epub_zip.read(package_file_path).decode("utf-8")
-            self._package = Package(package_xml_content)
-
     @property
     def package(self) -> Package:
         if self._package is None:
             rootfile_path = self.container.rootfile_path
-            self._unzip_and_parse_package(rootfile_path)
+            package_xml_content = self._read_file_from_epub(rootfile_path)
+            self._package = Package(package_xml_content)
         return self._package
     
+    @property
     def toc(self):
-        #TODO
-        return None
+        if self._toc is None:
+            package = self.package
+
+            if package.nav_href is not None:
+                toc_href = package.nav_href
+            elif package.toc_href is not None:
+                toc_href = package.toc_href
+
+            toc_xml_content = self._read_file_from_epub(toc_href)
+            self._toc = TableOfContents(toc_xml_content)
+        return self._toc
