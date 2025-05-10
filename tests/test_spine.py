795f534fb6a0c6e5a2743b0c64a31207ac52398d
Ernesto F. González <ernestogonzalezz@yahoo.com>
2025-05-10 17:06:56 +0200

    Add spine parsing

---
+++ tests/test_spine.py
@@ -0,0 +1,46 @@
+import pytest
+
+from epub_utils.package.spine import Spine
+from epub_utils.exceptions import ParseError
+
+VALID_SPINE_XML = """
+<spine toc="ncx" page-progression-direction="ltr">
+    <itemref idref="cover" linear="no"/>
+    <itemref idref="nav" linear="yes"/>
+    <itemref idref="chapter1" properties="page-spread-left"/>
+    <itemref idref="chapter2"/>
+</spine>
+"""
+
+MINIMAL_SPINE_XML = """
+<spine>
+    <itemref idref="content"/>
+</spine>
+"""
+
+def test_spine_initialization():
+    spine = Spine(VALID_SPINE_XML)
+    
+    assert spine.toc == "ncx"
+    assert spine.page_progression_direction == "ltr"
+    assert len(spine.itemrefs) == 4
+    
+    # Test first itemref (cover)
+    assert spine.itemrefs[0]["idref"] == "cover"
+    assert spine.itemrefs[0]["linear"] == False
+    assert spine.itemrefs[0]["properties"] == []
+    
+    # Test third itemref (chapter1)
+    assert spine.itemrefs[2]["idref"] == "chapter1"
+    assert spine.itemrefs[2]["linear"] == True
+    assert spine.itemrefs[2]["properties"] == ["page-spread-left"]
+
+def test_minimal_spine():
+    spine = Spine(MINIMAL_SPINE_XML)
+    
+    assert spine.toc is None
+    assert spine.page_progression_direction == "default"
+    assert len(spine.itemrefs) == 1
+    assert spine.itemrefs[0]["idref"] == "content"
+    assert spine.itemrefs[0]["linear"] == True
+    assert spine.itemrefs[0]["properties"] == []
